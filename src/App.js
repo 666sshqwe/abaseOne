36a585a77f040ad772c4e43aaeb65ee064e9c2d2
lhr <lihr@broada.com>
2022-08-02 12:50:23 +0800

    私有仓——react初始化项目

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import './App.css';
+import React,{Component} from 'react';
+import {Route,Redirect,BrowserRouter,Routes} from "react-router-dom";
+import Home from './Components/testPages/Home'
+import Menu from './Components/testPages/Menu'
+import Tool from './Components/testPages/ManyTool'
+
+export default class App extends Component{
+    render(){
+        return (
+            <BrowserRouter>
+                <div>
+                    <hr />
+                    <Routes>
+                    <Route exact path="/home" element={<Home/>}></Route>
+                    <Route  path="/menu"  element={<Menu/>}></Route>
+                    <Route  path="/tool"  element={<Tool/>}></Route>
+                    </Routes>
+                </div>
+            </BrowserRouter>
+        );
+    }
 }
 
-export default App;
---
+++ src/Components/testPages/Home.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import './../../index.css';
+import Mainpage from './../Mainpage';
+
+
+export default function Home() {
+    return (
+        <Mainpage tag="Home主页"/>
+    )
+}
+
